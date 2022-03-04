b4731a29327decbed94ddc5f1f9ad23ed0520a8a
lnagler1 <lnagler1@htl-steyr.ac.at>
2022-03-04 09:35:37 +0100

    Nu ned fertig

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello World ");
+        try {
+            PhoneNumber p1 = new PhoneNumber(42, 676, 3704878);
+            p1.isValid(p1);
+        } catch (IllegalPhoneNumberException e) {
+            e.type
+            e.printStackTrace();
+        }
+
     }
-}
+    }
